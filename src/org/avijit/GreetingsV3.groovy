e9f487ad76ca1ad78e74b1a172793aa8d1438d40
Avijit Pal <avijit.pal2@tesco.com>
2025-04-21 08:37:55 +0530

    use shared lib

--- src/org/avijit/GreetingsV3.groovy
+++ src/org/avijit/GreetingsV3.groovy
@@ -6,7 +6,7 @@ class GreetingsV3 {
     static def greet(Object script, String msg) {
         def quote =  new Quotes()
         script.echo "GreetingV3: ${msg}"
-        script.echo "Quotes:" + quote.generate()
+        script.echo "Quotes: " + quote.generate()
     }
 
 }
